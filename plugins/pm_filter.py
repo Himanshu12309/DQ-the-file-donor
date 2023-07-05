2fea3f763bfb70918e6bf949a416adfe8ea919f6
niteshkumar46 <132472966+niteshkumar46@users.noreply.github.com>
2023-07-05 09:26:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -4,7 +4,7 @@ import re
 import ast
 import math
 import random
-lock = asyncioLock()
+lock = asyncio.Lock()
 
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
